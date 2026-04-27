b2efa13f49ae0975ef484da65a13c3444c72b638
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-27 16:19:57 +0900

    Update storage.py

--- storage.py
+++ storage.py
@@ -3,6 +3,8 @@ with open("day_1_understanding.txt", "w", encoding="utf-8") as f:
     f.write("test practice")
 
 
+with open("day_1_understanding.txt", "r", encoding="utf-8") as f:
+    read = f.read()
 
 print(read)
 
