1528b770733b667ab40de6475c56ddc85e7664d8
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-27 16:19:36 +0900

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,3 +1,10 @@
-expenses = {
-    "coffee": 4,
-}
+
+with open("day_1_understanding.txt", "w", encoding="utf-8") as f:
+    f.write("test practice")
+
+
+with open("day_1_understanding.txt", "r", encoding="utf-8") as f:
+    read = f.read()
+
+print(read)
+
