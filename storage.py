ea9d33d352e3a1b69a7bf2f0dbf9c55869fd2414
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-28 16:55:42 +0900

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,10 +1,31 @@
 
-with open("day_1_understanding.txt", "w", encoding="utf-8") as f:
-    f.write("test practice")
+expenses = {}
+
+def save_expenses():
+    with open("expenses_data.txt", "w", encoding="utf-8") as f:
+        for product_name, amount in expenses.items():
+            f.write(f"{product_name}{amount}\n")
 
 
-with open("day_1_understanding.txt", "r", encoding="utf-8") as f:
-    read = f.read()
+    
+def load_expenses():
+    loaded = {} 
 
-print(read)
+    try:
+        with open("expenses_data.txt", "r", encoding="utf-8") as f:
+            for line in f:
+                line = f.strip()
+                #got this wrong , neeeds to be line.strip() its each line in the variable loop 
+                if line == "":
+                    continue
+
+
+            
+            product_name, amount_text = line.split(",",1)
+            loaded[product_name] = int[amount_text]
+
+    except FileNotFoundError:
+        pass
+
+    return loaded 
 
