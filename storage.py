0a8b33a62315521db898dfcdf7a92877bd6df20b
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-28 16:56:40 +0900

    Update storage.py

--- storage.py
+++ storage.py
@@ -7,6 +7,18 @@ def save_expenses():
             f.write(f"{product_name}{amount}\n")
 
 
+    
+def load_expenses():
+    loaded = {} 
+
+    try:
+        with open("expenses_data.txt", "r", encoding="utf-8") as f:
+            for line in f:
+                line = f.strip()
+                #got this wrong , neeeds to be line.strip() its each line in the variable loop 
+                if line == "":
+                    continue
+
 
             
             product_name, amount_text = line.split(",",1)
