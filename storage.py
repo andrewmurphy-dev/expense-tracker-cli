40f4ae5dfa67813268e85f97521d89221a9eb23e
Andrew Murphy <legionastartesofficial@gmail.com>
2026-04-29 19:22:41 +0900

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,31 +1,42 @@
+expenses = {
+    "coffee": 300,
+    "train": 220,
+    "food": 800
+}
 
-expenses = {}
+
 
 def save_expenses():
     with open("expenses_data.txt", "w", encoding="utf-8") as f:
         for product_name, amount in expenses.items():
-            f.write(f"{product_name}{amount}\n")
+            f.write(f"{product_name},{amount}\n")
 
 
-    
 def load_expenses():
-    loaded = {} 
+    loaded = {}
 
     try:
         with open("expenses_data.txt", "r", encoding="utf-8") as f:
             for line in f:
-                line = f.strip()
-                #got this wrong , neeeds to be line.strip() its each line in the variable loop 
+                line = line.strip()
                 if line == "":
                     continue
 
-
-            
-            product_name, amount_text = line.split(",",1)
-            loaded[product_name] = int[amount_text]
+                product_name, amount = line.split(",", 1)
+                #this actually comes from , for line in f 
+                #for line in f is reading in text format 
+                loaded[product_name] = int(amount)
 
     except FileNotFoundError:
         pass
 
-    return loaded 
+    return loaded
+
+
+
+
+save_expenses()
+
+loaded_data = load_expenses()
+print(loaded_data)
 
