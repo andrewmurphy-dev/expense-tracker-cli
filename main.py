e1e19f71e83e5a957d03a5c25901a74bcb72a487
Andrew Murphy <legionastartesofficial@gmail.com>
2026-05-06 14:07:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
+from storage import load_expenses, save_expenses
 from utils import add_expenses, show_expenses, show_total, delete_expenses
+
 
 def cli_expenses():
     expenses = load_expenses()
 
     while True:
-        print("welcome to Command Line Interface: User expenses")
-        print("type the following to choose your option")
-
-        print("press 1: Add expenses")
-        print("press 2: Show expenses")
-        print("press 3: Show total")
-        print("press 4: to delete expenses")
-        print("press: `exit` to end")
+        print("Welcome to Command Line Interface: User Expenses")
+        print("Type the following to choose your option")
+        print("Press 1: Add expense")
+        print("Press 2: Show expenses")
+        print("Press 3: Show total")
+        print("Press 4: Delete expense")
+        print("Type exit: End program")
 
         user_input = input("->: ").lower().strip()
 
         if user_input == "":
-            print("error, cannot be blank, please try again!")
+            print("Error: cannot be blank. Please try again.")
             continue
 
         elif user_input == "1":
-            add_expenses()
+            add_expenses(expenses)
+            save_expenses(expenses)
 
         elif user_input == "2":
-            show_expenses()
+            show_expenses(expenses)
 
         elif user_input == "3":
-            show_total()
+            show_total(expenses)
 
         elif user_input == "4":
-            delete_expenses()
-
+            delete_expenses(expenses)
+            save_expenses(expenses)
 
         elif user_input == "exit":
-            print("cli_expensis: thank you for your time, goodbye!")
+            print("cli_expenses: thank you for your time, goodbye!")
             break
 
         else:
-            print("error, start again!")
-            continue
-
+            print("Error: invalid option. Try again.")
 
 
 if __name__ == "__main__":
     cli_expenses()
-
